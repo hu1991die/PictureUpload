78853d737d0b25c03d4a2427ba7f96082199a878
ruanpeng <ruanpengwj43467@touna.cn>
2017-06-21 10:47:14 +0800

    second commit

--- src/main/java/com/feizi/controller/PictureController.java
+++ src/main/java/com/feizi/controller/PictureController.java
@@ -13,13 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by feizi on 2017/6/18.
  */
 @RestController
+@RequestMapping("/api")
 public class PictureController {
 
     @Autowired
     private PictureService pictureService;
 
-    @RequestMapping(value = "/api/picture", method = RequestMethod.GET)
+    @RequestMapping(value = "/picture", method = RequestMethod.GET)
     public Picture findPicturebyId(@RequestParam(value = "id", required = true) Integer id){
         return pictureService.findPicturebyId(id);
+    }
+
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET)
+    public String hello(@RequestParam(value = "id", required = true) Integer id){
+        return "Hello World!" + id;
     }
 }

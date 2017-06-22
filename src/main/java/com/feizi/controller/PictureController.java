3f79aa7b820320255930013ce682a328035d221a
ruanpeng <ruanpengwj43467@touna.cn>
2017-06-22 18:02:43 +0800

    modify controller

--- src/main/java/com/feizi/controller/PictureController.java
+++ src/main/java/com/feizi/controller/PictureController.java
@@ -4,15 +4,16 @@ import com.feizi.domain.Picture;
 import com.feizi.service.PictureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 图片上传Controller控制类
  * Created by feizi on 2017/6/18.
  */
 @Controller
-@RequestMapping("/api")
 public class PictureController {
 
     @Autowired
